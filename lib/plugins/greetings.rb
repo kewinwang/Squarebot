cf47493321f1f4927347ed2afb08e29a1ef4e8c2
matthew <matthew@foursquare.com>
2011-02-09 09:41:21 -0500

    added user caching to greeting engine

--- lib/plugins/greetings.rb
+++ lib/plugins/greetings.rb
@@ -3,8 +3,6 @@ class Greetings < Squarebot::Plugin
   register self, "hello/morning/hi/good day/good afternoon/good morning", "morning/afternoon <name>"
 
   def initialize
-    @messages = 0
-
   end
 
   def respond(message, user, options)
@@ -14,10 +12,11 @@ class Greetings < Squarebot::Plugin
   def react(message, user, options)
     @greetings = ['hello', 'good morning', 'morning', 'good afternoon', 'gday', 'howdy', "g'day", 'hi', 'afternoon', 'bonjour', 'ayup']
     @responses = ['howdy', "g'day", 'bonjour', 'ayup', 'nice of you to show up']
-    
+    @users ||= {}
     if @greetings.include?(message.downcase)
       puts "user: #{user}"
-      u = Campfire.user(user)
+      #this means I cache user info so I don't have to make multiple calls for the same user. SMRT smart
+      u = @users[user] ||= Campfire.user(user)
       return "#{@responses.sample} #{u["name"].split()[0]}"
     end
       
