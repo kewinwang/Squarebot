557c95f6ef125c42de70b38fd0f672562f655402
matthew <matthew@foursquare.com>
2011-04-29 17:03:53 -0400

    once per hour, not to hudson

--- lib/plugins/twss.rb
+++ lib/plugins/twss.rb
@@ -6,6 +6,7 @@ class Twss < Squarebot::Plugin
   def initialize
     @messages = 0
     TWSS.threshold = 5.0
+    @last_hour = -1
   end
 
   def respond(message, user, options)
@@ -14,9 +15,15 @@ class Twss < Squarebot::Plugin
   
   def react(message, user, options)
     @users = {}
+    
     u = @users[user] ||= Campfire.user(user)
+    return nil if @last_hour == Time.now.hour
     return nil if message.downcase.match(/that'?s what she said/)
-    return "#{u['name']}: That's what she said." if TWSS(message)
+    return nil if u['name'].downcase == 'hudson'
+    return nil if !TWSS(message.downcase)
+    #now we know that twss == true
+    @last_hour = Time.now.hour
+    return "#{u['name']}: That's what she said."
   end
   
 end
